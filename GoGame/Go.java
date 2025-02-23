5ea4e3b03baf6e2edd1769f6d299715895410617
Yuki Ramu <hailam2034@gmail.com>
2025-02-23 17:58:22 +0700

    Amen

---
+++ GoGame/Go.java
@@ -0,0 +1,8 @@
+package GoGame;
+import Graphics.GameFrame;
+public class Go {
+    public static void main(String[] args) {
+        GameFrame gf = new GameFrame();
+//        gf.start();
+    }
+}
